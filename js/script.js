0ad2edda5a48d7340d51a2cd963361eb7bb2dec7
Fernando Romero <klaatuhorror@gmail.com>
2024-02-24 15:57:46 +0100

    Nando y Karla

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const opciones = ["Piedra", "Papel", "Tijera"]; 
+
+let puntosUsuario = 0; 
+let puntosOrdenador = 0; 
+
+const btnpiedra = document.querySelector('btnpiedra');
+
+
+console.log('btnpiedra');
+
+botonJugada.addEventListener('click', function() {
+    return 
+
+}
+
+
+)
+
+
+let jugadaOrdenador = opciones [Math.floor(Math.random() * opciones.length) ]; 
+console.log ("La elección deL ORDENADOR  es: " + jugadaOrdenador); 
+
+
+
+//FUNCIONES 
+
+
+function obtenerResultado ( ){
+
+}
+
+function mostrarResultado ( ){
+
+
+}
+
+function actualizarPuntuacion ( ) {
+
+
+
+}
+
